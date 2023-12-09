75d053c073554f46a61ea1fb700d1ca74d0a2b59
RedWn <redwaniscool@gmail.com>
2023-12-09 21:44:51 +0300

    Update File.php

--- app/Models/File.php
+++ app/Models/File.php
@@ -7,8 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class File extends Model
 {
-    // use HasFactory;
-
+    use HasFactory;
     protected $table = "File";
     protected $fillable = [
         'projectPath',
