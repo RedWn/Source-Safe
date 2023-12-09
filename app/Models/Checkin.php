f4e14dc7c6072996f1eb83f08254aaf73ff583ba
RedWn <redwaniscool@gmail.com>
2023-12-09 21:44:54 +0300

    Create Checkin.php

---
+++ app/Models/Checkin.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Checkin extends Model
+{
+    use HasFactory;
+    protected $table = "Checkin";
+    protected $fillable = [
+        'date',
+        'fileID',
+        'duration',
+        'userID',
+        'checkedOut',
+    ];
+}
