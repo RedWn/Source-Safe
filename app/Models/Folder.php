affa6f21f3bde22c3b13dbf6f6d8cd3b5d2a65ba
RedWn <redwaniscool@gmail.com>
2023-12-10 22:48:26 +0300

    Update database

--- app/Models/Folder.php
+++ app/Models/Folder.php
@@ -5,13 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class File extends Model
+class Folder extends Model
 {
     use HasFactory;
+
     protected $fillable = [
-        'serverPath',
         'name',
-        'checked',
         'projectID',
         'folderID',
     ];
--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,14 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class File extends Model
+class Project extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'serverPath',
         'name',
-        'checked',
-        'projectID',
-        'folderID',
     ];
 }
