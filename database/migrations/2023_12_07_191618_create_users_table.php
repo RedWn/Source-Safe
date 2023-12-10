989dbf29222199715354adc6b916569ff94db4c7
Hasan Mothaffar <hasan.mozafar@gmail.com>
2023-12-10 10:09:51 +0300

    Rename 'User' to 'Users' in DB migration

--- database/migrations/2023_12_07_191618_create_users_table.php
+++ database/migrations/2023_12_07_191618_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUserTable extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
