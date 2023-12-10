4fd5ae03f2e7b563909e8f1ff1033662fbe63a3f
Hasan Mothaffar <hasan.mozafar@gmail.com>
2023-12-10 16:37:05 +0300

    Add a generic error handler to model not found errors.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
 
 class Handler extends ExceptionHandler
 {
@@ -26,5 +28,29 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof ModelNotFoundException) {
+            // Sample message: "No query results for model [App\Models\File] 1, 2, 3, 4"
+            // Extract model name and the rest of the message after the last bracket into the error message;
+            $originalMessage = $exception->getMessage();
+
+            preg_match('/\[(.*?)\]/', $originalMessage, $matches);
+            $contentBetweenBrackets = $matches[1];
+
+            $exploded = explode("\\", $contentBetweenBrackets);
+            $modelName = end($exploded);
+
+            $contentAfterLastBracket = substr($originalMessage, strrpos($originalMessage, "]") + 1);
+
+            $message = "Entry for $modelName $contentAfterLastBracket not found.";
+            return response()->json([
+                'message' => $message
+            ], 404);
+        }
+
+        return parent::render($request, $exception);
     }
 }
