0ef44bd024b3acb4fa5ee97090bace3b1da0c758
Hasan Mothaffar <hasan.mozafar@gmail.com>
2023-12-26 16:22:57 +0300

    Upload missing files

---
+++ app/Http/Resources/ProjectResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Project;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProjectResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        /** @var Project $project */
+        $project = $this->resource;
+
+        return [
+            'id' => $project->id,
+            'name' => $project->name
+        ];
+    }
+}
---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        /** @var User $user */
+        $user = $this->resource;
+
+        return [
+            'id' => $user->id,
+            'username' => $user->username
+        ];
+    }
+}
